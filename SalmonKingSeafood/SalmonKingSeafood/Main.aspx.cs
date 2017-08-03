22ffd90bc4f2689b6031fe771fd2e668103bbb0e
Sarah Park <parks96@uw.edu>
2017-08-03 16:31:10 -0700

    added menu items-- still needs pages to go to

---
+++ SalmonKingSeafood/SalmonKingSeafood/Main.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SalmonKingSeafood
+{
+    public partial class Main : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void menuBar_MenuItemClick(object sender, MenuEventArgs e)
+        {
+            var value = e.Item.Value;
+            if (value == "Exit")
+            {
+                System.Environment.Exit(1);
+            }
+            // Redirect to respective page
+            // ie -> click About goes to About.aspx
+            Response.Redirect(e.Item.Value + ".aspx");
+        }
+    }
+}z
