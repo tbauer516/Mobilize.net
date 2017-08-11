b31bc20168ac86a596aee048f206663d6e7d3849
tbauer516 <tcbauer516@gmail.com>
2017-08-11 10:40:27 -0700

    fixed js ajax template to be more concise

--- SalmonKingSeafood/SalmonKingSeafood/public/sqltest/sqltest.js
+++ SalmonKingSeafood/SalmonKingSeafood/public/sqltest/sqltest.js
@@ -13,20 +13,23 @@ const request = {
 
 let getTestSql = () => {
     return fetch('/BackEnd.asmx/SQLTest', request)
-        .then((response) => {
+        .then(response => {
             console.log(response);
             return response.json();
         })
-        .then((data) => {
+        .then(data => {
             console.log(data);
             return data.d;
         })
-        .catch((err) => {
+        .catch(err => {
             console.log(err);
         })
         .then(data => {
             let testDiv = document.querySelector('#sqltestdata');
             //testDiv.innerHTML = data;
             console.log(data);
-        });
+        })
+        .catch(err => {
+            console.log(err);
+        })
 };
