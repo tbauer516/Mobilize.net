6adc6b82d3e49e9896f2f9f8f9f2d5049d870bd6
Tyler Bauer <tbauer516@users.noreply.github.com>
2017-08-17 20:55:12 -0700

    tbauer516 create order html (#14)
    * add order page initial code. upgraded jquery and bootstrap to latest versions
    
    * add html code to the create order page to mimic most of the ui from the VB app
    
    * add bottom inputs, as well as change product radios to checkboxes
    
    * update gitignore

--- SalmonKingSeafood/SalmonKingSeafood/public/app.js
+++
@@ -1,9 +0,0 @@
-﻿// Toggle hamburger menu 
-var navbarContent = document.getElementById('navbar-content');
-document.getElementById('hamburger-button').addEventListener("click", function () {
-    if (navbarContent.style.display == "block") {
-        navbarContent.style.display = "none";
-    } else {
-        navbarContent.style.display = "block";
-    }
-});
