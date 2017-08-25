0324b6a9fe5f865f4fe2b896dd4ad67e84dec187
Sarah Park <parks96uw@users.noreply.github.com>
2017-08-25 09:47:24 -0700

    supplier class added (#19)

--- SalmonKingSeafood/SalmonKingSeafood/BackEnd.asmx.cs
+++ SalmonKingSeafood/SalmonKingSeafood/BackEnd.asmx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SalmonKingSeafood.suppliers;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -74,84 +75,16 @@ namespace SalmonKingSeafood
         [ScriptMethod(UseHttpGet = true, ResponseFormat = ResponseFormat.Json)]
         public string ViewSupplier()
         {
-            using (System.Data.SqlClient.SqlConnection dbconnect = new SqlConnection(ConfigurationManager.ConnectionStrings["SKSData"].ToString()))
-            {
-                var results = new List<Dictionary<string, object>>();
-                string cmdString = "SELECT * FROM viewSupplierInfo"; 
-                SqlCommand SupplierCMD = new SqlCommand(cmdString, dbconnect);
-                dbconnect.Open();
-
-                using (SqlDataReader reader = SupplierCMD.ExecuteReader())
-                {
-                    if (reader.HasRows)
-                    {
-                        while (reader.Read())
-                        {
-                            var item = new Dictionary<string, object>();
-                            for (int i = 0; i < reader.FieldCount; i++)
-                            {
-                                item.Add(reader.GetName(i), reader.IsDBNull(i) ? null : reader.GetValue(i));
-                            }
-                            results.Add(item);
-                        }
-                    }
-                }
-                dbconnect.Close();
-                Context.Response.Clear();
-                Context.Response.ContentType = "application/json";
-                return new JavaScriptSerializer().Serialize(results);
-            }
+            Supplier supplier = new Supplier();
+            return supplier.getSupplierView(Context);
         }
 
         [WebMethod]
         [ScriptMethod(UseHttpGet = false, ResponseFormat = ResponseFormat.Json)]
         public string SupplierMethod(String[] supplierForm)
         {
-            using (System.Data.SqlClient.SqlConnection dbconnect = new SqlConnection(ConfigurationManager.ConnectionStrings["SKSData"].ToString()))
-            {
-                String[] supplierInfo = { "SupplierName", "PaymentTerms", "Notes", "Fname", "Lname", "Email", "Phone", "Title", "Extension", "Fax", "BillingAddress", "City", "State", "Zipcode", "Country" };
-                String[] deleteInfo = { "SupplierName", "Fname", "Lname", "Title", "BillingAddress", "City", "State", "Zipcode", "Country" };
-                if (supplierForm[0] == "Delete")
-                {
-                    supplierInfo = deleteInfo;
-                }
-
-                var cmdString = "exec usp" + supplierForm[0] + "Supplier @" + supplierInfo[0] + " = '" + supplierForm[1] + "'";
-                var results = new List<Dictionary<string, object>>();
-
-                for (int i = 1; i < supplierInfo.Length; i++)
-                {
-                    cmdString += " , @" + supplierInfo[i] + " = '" + supplierForm[i + 1] + "'";
-                }
-
-                SqlCommand insertProductCmd = new SqlCommand(cmdString, dbconnect);
-                for (int i = 0; i < supplierInfo.Length; i++)
-                {
-                    insertProductCmd.Parameters.AddWithValue(supplierInfo[i], supplierForm[i + 1]);
-                }
-                dbconnect.Open();
-
-                using (SqlDataReader reader = insertProductCmd.ExecuteReader())
-                {
-                    if (reader.HasRows)
-                    {
-                        while (reader.Read())
-                        {
-                            var item = new Dictionary<string, object>();
-                            for (int i = 0; i < reader.FieldCount; i++)
-                            {
-                                item.Add(reader.GetName(i), reader.IsDBNull(i) ? null : reader.GetValue(i));
-                            }
-                            results.Add(item);
-                        }
-                    }
-                }
-
-                dbconnect.Close();
-                Context.Response.Clear();
-                Context.Response.ContentType = "application/json";
-                return new JavaScriptSerializer().Serialize(results);
-            }
+            Supplier supplier = new Supplier();
+            return supplier.getSupplierMethod(Context, supplierForm);
         }
 
         [WebMethod]
