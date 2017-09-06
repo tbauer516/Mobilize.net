284b8b5b52af3b349f63f31279e3ab48f42fe468
mkkiyoi <mkkiyoi@gmail.com>
2017-09-06 11:08:35 -0700

    add Product.cs

---
+++ SalmonKingSeafood/SalmonKingSeafood/products/Product.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.Script.Services;
+using System.Web.Services;
+
+namespace SalmonKingSeafood
+{
+    public class Product
+    {
+
+        public static string ProductSQL(HttpContext Context, String[] data, String[] info)
+        {
+            using (System.Data.SqlClient.SqlConnection dbconnect = new SqlConnection(ConfigurationManager.ConnectionStrings["SKSData"].ToString()))
+            {
+                var results = new List<Dictionary<string, object>>();
+                string cmdString = "exec usp" + data[0] + "Product @" + info[0] + " = '" + data[1] + "'";
+                for (var i = 1; i < info.Length; i++)
+                {
+                    cmdString += ", @" + info[i] + " = '" + data[i + 1] + "'";
+                }
+                Console.WriteLine(cmdString);
+                SqlCommand addProductCmd = new SqlCommand(cmdString, dbconnect);
+                dbconnect.Open();
+                using (SqlDataReader reader = addProductCmd.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            var item = new Dictionary<string, object>();
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                item.Add(reader.GetName(i), reader.IsDBNull(i) ? null : reader.GetValue(i));
+                            }
+                            results.Add(item);
+                        }
+                    }
+                }
+
+                dbconnect.Close();
+
+                Context.Response.Clear();
+                Context.Response.ContentType = "application/json";
+                return new JavaScriptSerializer().Serialize(results);
+            }
+        }
+
+
+        public static string SQLViewProduct(HttpContext Context)
+        {
+            using (System.Data.SqlClient.SqlConnection dbconnect = new SqlConnection(ConfigurationManager.ConnectionStrings["SKSData"].ToString()))
+            {
+                var results = new List<Dictionary<string, object>>();
+
+                string cmdString = "SELECT * FROM tblPRODUCT";
+                SqlCommand FindProductCmd = new SqlCommand(cmdString, dbconnect);
+                dbconnect.Open();
+
+                using (SqlDataReader reader = FindProductCmd.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            var item = new Dictionary<string, object>();
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                item.Add(reader.GetName(i), reader.IsDBNull(i) ? null : reader.GetValue(i));
+                            }
+                            results.Add(item);
+                        }
+                    }
+                }
+                dbconnect.Close();
+                Context.Response.Clear();
+                Context.Response.ContentType = "application/json";
+                return new JavaScriptSerializer().Serialize(results);
+            }
+        }
+    }
+}
